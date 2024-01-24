376221dbc7b5dc5e22ee6336f7de8499bdcee0e9
nyyakko <nyakonyns@gmail.com>
2024-01-24 16:20:29 -0300

    descriptor/GDT: fix comma alignment

--- argon/source/system/descriptor/GDT.s
+++ argon/source/system/descriptor/GDT.s
@@ -4,17 +4,17 @@
 .global load_gdt
 .type load_gdt, @function
 load_gdt:
-    mov 4(%esp), %eax
+    mov 4(%esp) , %eax
     lgdt (%eax)
 
-    mov $0x10, %ax
-    mov %ax, %ds
-    mov %ax, %es
-    mov %ax, %fs
-    mov %ax, %gs
-    mov %ax, %ss
+    mov $0x10   , %ax
+    mov %ax     , %ds
+    mov %ax     , %es
+    mov %ax     , %fs
+    mov %ax     , %gs
+    mov %ax     , %ss
 
-    jmp $0x08, $flush_and_return
+    jmp $0x08   , $flush_and_return
 
 flush_and_return:
     ret
