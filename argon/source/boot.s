eecfc7483656d3ffaa80f107af5bb605e4bedcab
nyyakko <nyakonyns@gmail.com>
2024-01-24 16:29:08 -0300

    boot: fix comma alignment

--- argon/source/boot.s
+++ argon/source/boot.s
@@ -1,8 +1,8 @@
-.set ALIGN,    1<<0
-.set MEMINFO,  1<<1
-.set FLAGS,    ALIGN | MEMINFO
-.set MAGIC,    0x1BADB002
-.set CHECKSUM, -(MAGIC + FLAGS)
+.set ALIGN    , 1<<0
+.set MEMINFO  , 1<<1
+.set FLAGS    , ALIGN | MEMINFO
+.set MAGIC    , 0x1BADB002
+.set CHECKSUM , -(MAGIC + FLAGS)
 
 .section .note.GNU-stack
 
