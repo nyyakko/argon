ba579333f3ee30f83fadde472852e4bc801a1d7b
nyyakko <nyakonyns@gmail.com>
2024-01-24 16:34:25 -0300

    system/ISR: panic already ``cli`` and ``hlt``.

--- argon/source/system/ISR.cpp
+++ argon/source/system/ISR.cpp
@@ -30,148 +30,106 @@ void isr::install(IDT& idt)
 
 void isr::_0()
 {
-    asm ("cli");
     panic("divide by zero");
-    asm ("hlt");
 }
 
 void isr::_1()
 {
-    asm ("cli");
     panic("debug error");
-    asm ("hlt");
 }
 
 void isr::_2()
 {
-    asm ("cli");
     panic("nmi interrupt");
-    asm ("hlt");
 }
 
 void isr::_3()
 {
-    asm ("cli");
     panic("breakpoint");
-    asm ("hlt");
 }
 
 void isr::_4()
 {
-    asm ("cli");
     panic("overflow");
-    asm ("hlt");
 }
 
 void isr::_5()
 {
-    asm ("cli");
     panic("bound range exceeded");
-    asm ("hlt");
 }
 
 void isr::_6()
 {
-    asm ("cli");
     panic("invalid opcode exception");
-    asm ("hlt");
 }
 
 void isr::_7()
 {
-    asm ("cli");
     panic("device not available exception");
-    asm ("hlt");
 }
 
 void isr::_8()
 {
-    asm ("cli");
     panic("Double Fault Exception");
-    asm ("hlt");
 }
 
 void isr::_9()
 {
-    asm ("cli");
     panic("coprocessor segment overrun");
-    asm ("hlt");
 }
 
 void isr::_10()
 {
-    asm ("cli");
     panic("invalid tss exception");
-    asm ("hlt");
 }
 
 void isr::_11()
 {
-    asm ("cli");
     panic("segment not present");
-    asm ("hlt");
 }
 
 void isr::_12()
 {
-    asm ("cli");
     panic("stack fault exception");
-    asm ("hlt");
 }
 
 void isr::_13()
 {
-    asm ("cli");
     panic("general protection exception");
-    asm ("hlt");
 }
 
 void isr::_14()
 {
-    asm ("cli");
     panic("page fault exception");
-    asm ("hlt");
 }
 
 void isr::_16()
 {
-    asm ("cli");
     panic("fpu float-point error");
-    asm ("hlt");
 }
 
 void isr::_17()
 {
-    asm ("cli");
     panic("alignment check exception");
-    asm ("hlt");
 }
 
 void isr::_18()
 {
-    asm ("cli");
     panic("machine-check exception");
-    asm ("hlt");
 }
 
 void isr::_19()
 {
-    asm ("cli");
     panic("simd floating-point exception");
-    asm ("hlt");
 }
 
 void isr::_20()
 {
-    asm ("cli");
     panic("virtualization exception");
-    asm ("hlt");
 }
 
 void isr::_21()
 {
-    asm ("cli");
     panic("control protection exception");
-    asm ("hlt");
 }
 
